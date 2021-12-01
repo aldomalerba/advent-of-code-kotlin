9f30a59bd80abfdc35a211668c5eb2ffe3f54601
Aldo Malerba <aldomalerba.dev@gmail.com>
2021-12-01 22:26:36 +0100

    measurements contains only decreased values

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,2 +1,5 @@
 class Day1 {
+    fun execute(measurements: List<Int>): Int {
+        return measurements.filterIndexed { index, item -> if(index>0) item > measurements[index-1] else false }.size
+    }
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
 class Day1Test {
+
+    @Test
+    fun `execute returns zero when measurements contains one measurement`() {
+        val measurements = listOf(199)
+        val result = Day1().execute(measurements)
+        assertEquals(0, result)
+    }
+
+    @Test
+    fun `measurements contains only increased values`() {
+        val measurements = listOf(199,200,208)
+        val result = Day1().execute(measurements)
+        assertEquals(2, result)
+    }
+
+    @Test
+    fun `measurements contains only decreased values`() {
+        val measurements = listOf(199,198,197)
+        val result = Day1().execute(measurements)
+        assertEquals(0, result)
+    }
 }
