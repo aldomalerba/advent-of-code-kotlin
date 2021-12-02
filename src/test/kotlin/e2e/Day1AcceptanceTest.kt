8ff6fa6f6333120dd00f4ede75651b328544a0c5
Aldo Malerba <aldomalerba.dev@gmail.com>
2021-12-02 19:28:59 +0100

    refactor acceptance test

--- src/test/kotlin/e2e/Day1AcceptanceTest.kt
+++ src/test/kotlin/e2e/Day1AcceptanceTest.kt
@@ -1,4 +1,45 @@
 package e2e
 
+import Day1
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class Day1AcceptanceTest {
+
+    @Test
+    fun `happy path part 1`() {
+        val measurements = listOf(
+            "199",
+            "200",
+            "208",
+            "210",
+            "200",
+            "207",
+            "240",
+            "269",
+            "260",
+            "263"
+        )
+        val result = Day1().execute(measurements, 1)
+        Assertions.assertEquals(7, result)
+    }
+
+    @Test
+    fun `happy path part 2`() {
+        val measurements = listOf(
+            "199",
+            "200",
+            "208",
+            "210",
+            "200",
+            "207",
+            "240",
+            "269",
+            "260",
+            "263"
+        )
+
+        val result = Day1().execute(measurements,3)
+        Assertions.assertEquals(5, result)
+    }
 }
