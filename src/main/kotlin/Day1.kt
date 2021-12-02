f3a40eef16c48350619491ef73815ed2ac4da6ec
Aldo Malerba <aldo.malerba@it.clara.net>
2021-12-02 08:31:27 +0100

    day1 refactored

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,13 +1,11 @@
 class Day1 {
+
     fun execute(measurements: List<Int>, slicingSize: Int): Int {
-        var measurements = groupSlicingWindows(measurements, slicingSize)
-        return measurements.drop(1).filterIndexed { index, item -> item > measurements[index] }.size
+        measurements.grouped(slicingSize).let {
+            return it.increasingValues().size
+        }
     }
 
-    private fun groupSlicingWindows(measurements: List<Int>, slicingSize: Int) =
-        (0..(measurements.size - slicingSize)).map {
-                index -> measurements.sumSlicingWindows(index, slicingSize)
-        }
-
-    private fun List<Int>.sumSlicingWindows(index: Int, slicingSize: Int) = drop(index).take(slicingSize).sum()
+    private fun List<Int>.grouped(slicingSize: Int) = windowed(slicingSize).map { it.sum() }
+    private fun List<Int>.increasingValues() = drop(1).filterIndexed { index, item -> item > this[index] }
 }
