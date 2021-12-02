2bd63f6be101fe79c2b8c17bbaed48c4ec26a52a
Aldo Malerba <aldo.malerba@it.clara.net>
2021-12-02 08:10:28 +0100

    updated group slicing windows

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,14 +1,11 @@
 class Day1 {
-    fun execute(measurements: List<Int>, slicingSize: Int = 1): Int {
-
-        groupSlicingWindows(measurements, slicingSize).also {
-            return it.drop(1).filterIndexed { index, item -> item > it[index] }.size
-        }
-
+    fun execute(measurements: List<Int>, slicingSize: Int): Int {
+        var measurements = groupSlicingWindows(measurements, slicingSize)
+        return measurements.drop(1).filterIndexed { index, item -> item > measurements[index] }.size
     }
 
     private fun groupSlicingWindows(measurements: List<Int>, slicingSize: Int) =
-        List(measurements.size - slicingSize + 1 ) {
+        (0..(measurements.size - slicingSize)).map {
                 index -> measurements.sumSlicingWindows(index, slicingSize)
         }
 
