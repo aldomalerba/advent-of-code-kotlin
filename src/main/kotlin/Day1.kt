fd87ab82865d091f3c7b78dccd71c7c16550261f
Aldo Malerba <aldo.malerba@it.clara.net>
2021-12-02 16:39:59 +0100

    refactor mains

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -1,11 +1,21 @@
+import java.io.File
+
 class Day1 {
 
-    fun execute(measurements: List<Int>, slicingSize: Int): Int {
-        measurements.grouped(slicingSize).let {
+    fun execute(measurements: List<String>, slicingSize: Int): Int {
+        measurements.map { it.toInt() }.grouped(slicingSize).let {
             return it.increasingValues().size
         }
     }
 
     private fun List<Int>.grouped(slicingSize: Int) = windowed(slicingSize).map { it.sum() }
     private fun List<Int>.increasingValues() = drop(1).filterIndexed { index, item -> item > this[index] }
+}
+
+fun main() {
+
+    val input = File("src","Day1.txt").readLines().map { it.trim() }
+    println("output part 1: ${Day1().execute(input, 1)}")
+    println("output part 2: ${Day1().execute(input, 3)}")
+
 }
--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,3 +1,5 @@
+import java.io.File
+
 class Day2(private val submarine: MoveSubmarine) {
 
     fun execute(commands: List<String>) : Int {
@@ -10,5 +12,13 @@ class Day2(private val submarine: MoveSubmarine) {
 
     private fun String.steps() = split(" ").last().toInt()
     private fun String.direction() = split(" ").first()
+
+}
+
+fun main() {
+
+    val input = File("src","Day2.txt").readLines().map { it.trim() }
+    println("output part 1: ${Day2(Submarine()).execute(input)}")
+    println("output part 2: ${Day2(AimSubmarine()).execute(input)}")
 
 }
