23fb06a623d8b7ce1deefee195da7d0b8c485a29
Aldo Malerba <aldomalerba.dev@gmail.com>
2021-12-05 12:21:26 +0100

    day4

--- src/main/kotlin/day4/BingoSubsystem.kt
+++ src/main/kotlin/day4/BingoSubsystem.kt
@@ -9,15 +9,13 @@ class BingoSubsystem {
 
     fun play(input: List<String>) : List<Int> {
 
-        val numbers = input.first().split(",")
-        var boards = input.drop(1).filter { it.isNotEmpty() }.windowed(5,5).map {
-            it.map { it.split(" +".toRegex()) }
-        }.toMutableList()
+        val numbers = toNumbers(input)
+        var boards = toBoards(input).toMutableList()
 
         for(number in numbers) {
-            boards = boards.map { board -> board.map{ row -> row.map { if(it==number) "X" else it  } } }.toMutableList()
+            boards = boards.map { board -> board.marked(number) }.toMutableList()
 
-            val winBoards = boards.filter { board-> anyMarkedRow(board) || anyMarkedRow(board.pivot()) }
+            val winBoards = boards.filter { board-> board.containsMarkedRow() || board.pivot().containsMarkedRow() }
             winners.addAll(winBoards.map { it.score(number) })
             boards.removeAll(winBoards)
 
@@ -26,10 +24,16 @@ class BingoSubsystem {
         return winners
     }
 
-    private fun anyMarkedRow(board: List<List<String>>) =
-        board.map { it.all { it == "X" } }.any { it }
+    private fun toNumbers(input: List<String>) = input.first().split(",")
 
+    private fun toBoards(input: List<String>) =
+        input.drop(1).filter { it.isNotEmpty() }.windowed(5, 5).map {
+            it.map { it.split(" +".toRegex()) }
+        }
+
+    private fun Board.containsMarkedRow() = map { it.all { it == "X" } }.any { it }
     private fun Board.pivot() = (indices).map { index -> map { it[index] } }
     private fun Board.score(number: String) = flatten().filter { it != "X" }.sumOf { it.toInt() } * number.toInt()
+    private fun Board.marked(number: String) = map { row -> row.map { if (it == number) "X" else it } }
 
 }
